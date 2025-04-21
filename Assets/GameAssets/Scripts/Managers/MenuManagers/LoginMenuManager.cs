d62a1a731dd6e7de8a5d366ff37a310589478768
mert coskun <mertt.coskunm@bitbucket.org>
2025-04-21 21:04:54 +0200

    added login menu manager

---
+++ Assets/GameAssets/Scripts/Managers/MenuManagers/LoginMenuManager.cs
@@ -0,0 +1,82 @@
+using TMPro; 
+using UnityEngine;
+using UnityEngine.UI;
+public class LoginMenuManager : MonoBehaviour
+{
+    [SerializeField]  private GameObject loginMenuObject;
+    [SerializeField]  private TMP_InputField  loginUserNameInputField;
+    [SerializeField]  private TMP_InputField  loginPasswordInputField;
+    [SerializeField]  private TMP_Text  loginErrorText;
+    [SerializeField]  private Button loginButton;
+    [SerializeField]  private Button signUpMenuButton;
+
+    //[SerializeField]  private GameObject signUpMenuObject;
+
+    [SerializeField]  private SignUpMenuManager signUpMenuManager;
+    [SerializeField]  private MainMenuManager mainMenuManager;
+    //[SerializeField]  private GameObject mainMenuObject;
+
+    [SerializeField]  private LoginClient loginClient;
+
+    
+
+
+    void Start()
+    {
+        if(loginButton != null)
+        {
+            loginButton.onClick.AddListener(OnLoginButtonClick); 
+        }
+
+        if(signUpMenuButton != null )
+        {
+            signUpMenuButton.onClick.AddListener(OnSignUpMenuButtonClick); 
+        }
+    }
+
+
+
+    void OnLoginButtonClick()
+    {
+        loginClient.OnResponse += OnLoginResponse;
+        Debug.Log(loginUserNameInputField.text);
+        Debug.Log(loginPasswordInputField.text);
+        loginClient.OnTryLogin(loginUserNameInputField.text, loginPasswordInputField.text);
+
+    }
+
+    void OnLoginResponse(string token, bool isSuccess){
+       if(isSuccess)
+       {
+        GameManager.Instance.SetAuthToken(token); 
+        Close();
+        mainMenuManager.Open(); 
+       }
+       else{
+        loginErrorText.text = "Invalid username or password"; 
+       } 
+    }
+
+    void OnSignUpMenuButtonClick()
+    {
+        Close();
+        signUpMenuManager.Open();     
+    }
+
+    public void Open()
+    {
+        loginUserNameInputField.text = "";
+        loginPasswordInputField.text = "";
+
+        loginMenuObject.SetActive(true); 
+
+    }
+    public void Close()
+    {
+        loginUserNameInputField.text = "";
+        loginPasswordInputField.text = "";
+
+        loginMenuObject.SetActive(false);
+
+    }
+}
